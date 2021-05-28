da18cb65a8f907bd91872ff03084da44b4765ebd
Shawn Littrel <shawnlittrel@protonmail.com>
2021-05-27 19:52:38 -0700

    configured server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+mongoose.connect('mongodb: //localhost/social-network', {
+     userFindAndModify: false,
+     useNewUrlParser: true,
+     useUnifiedTopology: true
+});
+
+mongoose.set('debug', true);
+
+app.use(require('./routes'));
+
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
