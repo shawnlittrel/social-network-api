d222a9c21bc095f23e11a329ba7a475e6726cb98
Shawn Littrel <shawnlittrel@protonmail.com>
2021-05-27 21:10:51 -0700

    finished fixing

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
-     userFindAndModify: false,
+     useFindAndModify: false,
      useNewUrlParser: true,
      useUnifiedTopology: true
 });
